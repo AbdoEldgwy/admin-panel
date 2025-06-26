3bcabdf8cfd64cdc2a6efdf0a9f1edbe24be137b
AbdoEldgwy <abdo.eldgwy1@gmail.com>
2025-06-26 09:33:27 +0300

    UPDATE

--- gradadmin/settings.py
+++ gradadmin/settings.py
@@ -146,4 +146,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = "abdo.eldgwy1@gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = "dnvu pmck lnds vesy"
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_PASSWORD")
